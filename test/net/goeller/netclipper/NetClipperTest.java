8973dbcb05b3a9c74c6051951854ed258a80f47a
Christian Goeller <cgoeller@gmx.de>
2006-04-04 19:45:33 +0000

    test added

---
+++ test/net/goeller/netclipper/NetClipperTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright by Christian Goeller
+ *
+ * $HeadURL:svn+ssh://z990/home/chris/Daten/Repositories/netclipboard/netclipboard/source/net/goeller/netclipper/INetClipboardListener.java $
+ * $LastChangedDate:2006-04-04 21:37:29 +0200 (Di, 04 Apr 2006) $
+ */
+package net.goeller.netclipper;
+
+import junit.framework.TestCase;
+
+/**
+ * @author $LastChangedBy:chris $
+ * @version $LastChangedRevision:26 $
+ */
+public class NetClipperTest extends TestCase
+{
+
+	
+	/**
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	/*
+	 * Test method for 'net.goeller.netclipper.NetClipper.addNetClipboardListener(INetClipboardListener)'
+	 */
+	public void testAddNetClipboardListener()
+	{
+
+	}
+
+	/*
+	 * Test method for 'net.goeller.netclipper.NetClipper.removeNetClipboardListener(INetClipboardListener)'
+	 */
+	public void testRemoveNetClipboardListener()
+	{
+
+	}
+
+	/*
+	 * Test method for 'net.goeller.netclipper.NetClipper.informListeners(String)'
+	 */
+	public void testInformListeners()
+	{
+
+	}
+
+}
