937564fa4af11d6b258d4c0bf2946ec665be2967
Christian Goeller <cgoeller@gmx.de>
2006-04-04 22:38:24 +0000

    updated scripts

--- start/start.cmd
+++ start/start.cmd
@@ -5,9 +5,7 @@ set PROJECT_HOME=E:\Workspace\netclipboard
 set LIB_HOME=%PROJECT_HOME%\libs
 
 set BOOT_CLASS=net.goeller.netclipper.NetClipperApp
-set BOOT_PARAM=-w
-set CLASSPATH=%PROJECT_HOME%\source\.;%LIB_HOME%\looks-2.0.1.jar;%LIB_HOME%\xmlrpc-2.0.1.jar;%LIB_HOME%\forms-1.0.6.jar;%LIB_HOME%\commons-codec-1.3.jar
+set BOOT_PARAM=
+set CLASSPATH=%PROJECT_HOME%\bin;%LIB_HOME%\looks-2.0.1.jar;%LIB_HOME%\xmlrpc-2.0.1.jar;%LIB_HOME%\forms-1.0.6.jar;%LIB_HOME%\commons-codec-1.3.jar
 
 "%JAVA_BIN%" -cp %CLASSPATH% %BOOT_CLASS% %BOOT_PARAM%
-  
-pause
--- start/start.sh
+++ start/start.sh
@@ -1,1 +1,10 @@
-java -cp libs/looks-2.0.1.jar:libs/xmlrpc-2.0.1.jar:libs/forms-1.0.6.jar:libs/commons-codec-1.3.jar:.  net.goeller.netclipper.NetClipperApp
+JAVA_BIN=java
+
+PROJECT_HOME=/home/chris/workspace/netclipboard
+LIB_HOME=$PROJECT_HOME/libs
+
+BOOT_CLASS=net.goeller.netclipper.NetClipperApp
+BOOT_PARAM=
+CLASSPATH=$PROJECT_HOME/bin:$LIB_HOME/looks-2.0.1.jar:$LIB_HOME/xmlrpc-2.0.1.jar:$LIB_HOME/forms-1.0.6.jar:$LIB_HOME/commons-codec-1.3.jar
+
+$JAVA_BIN -cp $CLASSPATH $BOOT_CLASS $BOOT_PARAM
